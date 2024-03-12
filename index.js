b291a38af98c142cf7f44eb1ebf9459bffc7c7b8
André Soares <99965775+and3d-byte@users.noreply.github.com>
2024-03-11 21:14:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const { createApp } = Vue
+
+createApp({
+  data(){
+    return{
+      display: '0',
+      numeroAtual: null,
+      numeroAnterior: null,
+      operador: null
+    }
+  },
+  methods:{
+    lidarBotao(botao){
+      switch(botao){
+        case "*":
+        case "-":
+        case "+":
+        case "/":
+          this.lidarOperador(botao)
+          break
+        case ".":
+          this.lidarDecimal()
+          break
+        case "=":
+          this.lidarIgual()
+          break
+        case "AC":
+          this.lidarClear()
+          break
+        default:
+          this.lidarNumero()
+      }
+    },
+    lidarOperador(botao){
+      if(this.operador !== null){
+        this.lidarIgual();
+      }
+      this.numeroAnterior = parseFloat(this.display);
+      this.operador = botao;
+    }
+  }
+}).mount("#app")
