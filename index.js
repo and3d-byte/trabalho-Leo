a0b80dd109b6d6b38f82ad338750f6436d44aa5e
André Soares <99965775+and3d-byte@users.noreply.github.com>
2024-03-15 13:42:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
-      display: '0',
-      numeroAtual: null,
-      numeroAnterior: null,
-      operador: null
-    }
-  },
-  methods:{
-    lidarBotao(botao){
-      switch(botao){
-        case "*":
-        case "-":
-        case "+":
-        case "/":
-          this.lidarOperador(botao)
-          break
-        case ".":
-          this.lidarDecimal()
-          break
-        case "=":
-          this.lidarIgual()
-          break
-        case "AC":
-          this.lidarClear()
-          break
-        default:
-          this.lidarNumero()
-      }
+    data() {
+        return {
+            display: '0',
+            numeroAtual: null,
+            numeroAnterior: null,
+            operador: null,
+            aguardandoOperacao: false
+        };
     },
-    lidarOperador(botao){
-      if(this.operador !== null){
-        this.lidarIgual();
-      }
-      this.numeroAnterior = parseFloat(this.display);
-      this.operador = botao;
+    methods: {
+        lidarBotao(botao) {
+            if (botao === 'AC') {
+                this.lidarClear();
+            } else if (botao === '=') {
+                this.lidarIgual();
+            } else if (['+', '-', '*', '/'].includes(botao)) {
+                this.lidarOperador(botao);
+            } else {
+                this.lidarNumero(botao);
+            }
+        },
+        lidarOperador(botao) {
+            if (this.operador !== null && !this.aguardandoOperacao) {
+                this.lidarIgual();
+            }
+            this.operador = botao;
+            this.aguardandoOperacao = true;
+            this.display = this.numeroAnterior !== null ? this.numeroAnterior + ' ' + this.operador : '0';
+        },
+        lidarNumero(numero) {
+            if (this.aguardandoOperacao) {
+                this.display = numero;
+                this.aguardandoOperacao = false;
+            } else {
+                this.display = this.display === '0' ? numero : this.display + numero;
+            }
+        },
+        lidarIgual() {
+            if (this.operador && !this.aguardandoOperacao) {
+                const operacao = this.numeroAnterior + ' ' + this.operador + ' ' + parseFloat(this.display);
+                this.display = eval(operacao);
+                this.operador = null;
+                this.numeroAnterior = null;
+                this.aguardandoOperacao = true;
+            }
+        },
+        lidarClear() {
+            this.display = '0';
+            this.numeroAtual = null;
+            this.numeroAnterior = null;
+            this.operador = null;
+            this.aguardandoOperacao = false;
+        }
     }
-  }
-}).mount("#app")
+}).mount("#app");
